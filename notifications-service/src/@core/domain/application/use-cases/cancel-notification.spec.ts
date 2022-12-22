5a00233a4f9120ab18d60288bce5fb17fa1a8828
Johnny Carreiro <johnnycarreiro@Johnnys-MacBook-Pro.local>
2022-12-22 00:38:18 -0300

    feat: Add Notification factory to Cancel Notification EC

--- notifications-service/src/@core/domain/application/use-cases/cancel-notification.spec.ts
+++ notifications-service/src/@core/domain/application/use-cases/cancel-notification.spec.ts
@@ -1,19 +1,14 @@
 import { InMamoryNotificationRepository } from '@test/stubs/in-mamory-notification-repository'
 import { CancelNotificationUseCase } from './cancel-notification'
-import { Content, Notification } from '@entities/notification'
 import { NotificationNotFoundException } from './errors/notification-errors'
+import { makeNotification } from '@test/factories/notification-factory'
 
 describe('Cancel Notification UC', () => {
   it('Should be able to cancel a notification', async () => {
     const repository = new InMamoryNotificationRepository()
     const cancelNotification = new CancelNotificationUseCase(repository)
 
-    const notification = Notification.create({
-      recipientId: 'recipient-id-uuid',
-      content: Content.create('Notification content').value as Content,
-      category: 'notification-category'
-    }).value as Notification
-
+    const notification = makeNotification()
     await repository.create(notification)
 
     await cancelNotification.execute({ notificationId: notification.id })
